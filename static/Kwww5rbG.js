bb2cd8710c5e91fb6d1221af7c9e2b31dd5d8b25
YueRay <ray18636399676@gmail.com>
2024-05-29 18:06:01 +0800

    test

---
+++ static/Kwww5rbG.js
@@ -0,0 +1,1 @@
+import{u as l}from"./horkCLns.js";import{U as n,m as p}from"./DcdTv0Ew.js";function i(e,a){const{title:s,titleTemplate:m,...o}=e;return l({title:s,titleTemplate:m,_flatMeta:o},{...a,transform(t){const r=n({...t._flatMeta});return delete t._flatMeta,{...t,meta:r}}})}const c=p("/img/laptop-frame.svg");export{c as _,i as u};
