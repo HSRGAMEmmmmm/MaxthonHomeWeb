50419e8142ab51a6c630484c48d039d1499470be
YueRay <ray18636399676@gmail.com>
2024-05-31 03:29:06 +0800

    test

---
+++ static/C2fibwM2.js
@@ -0,0 +1,1 @@
+import{u as s}from"./C9zuytRd.js";import{D as u}from"./BDMod4Qd.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return s({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=u({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{i as u};
