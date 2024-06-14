1f90d0aabf71f3f4de58a87ca61d8959fc7286bf
YueRay <ray18636399676@gmail.com>
2024-06-14 16:19:57 +0800

    test

---
+++ static/BVULvuOJ.js
@@ -0,0 +1,1 @@
+import{_ as l}from"./BChVxn9g.js";import{c as o,b as n,$ as r,F as s,o as c}from"./B4UW-wwy.js";const _={__name:"base",props:{lang:{type:String,default:"en"}},setup(a){return(t,m)=>{const e=l;return c(),o(s,null,[n(e,{name:"app-header",lang:a.lang},null,8,["lang"]),r(t.$slots,"default"),n(e,{name:"app-footer",lang:a.lang},null,8,["lang"])],64)}}};export{_ as default};
---
+++ static/CT5jQtHm.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./BfdAemGK.js";import"./B4UW-wwy.js";export{m as default};
